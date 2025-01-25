7543d2ed67749206c7b8c670622cb4273cc7c98e
Mariusz Wątroba <mariusz2723@gmail.com>
2025-01-25 19:57:11 +0100

    Created Rock.*

---
+++ include/Rock.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <windows.h>
+
+#include <utility>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+#include "Interceptor.h"
+#include "Game.h"
+
+
+class Rock: public sf::CircleShape{
+    //statyczne tekstury
+};
